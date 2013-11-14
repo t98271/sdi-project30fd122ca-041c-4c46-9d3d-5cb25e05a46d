b9b465636fa749d043e229e4c744288fc94e105e
Avery Bergara <maulventress@fullsail.edu>
2013-11-14 17:56:01 -0600

    variables set

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,8 @@
-alert("JavaScript works!");
+var numBros = 4;
+var nameProm = prompt("What should one of the brothers names be?", "john")
+var nameBros = ["chip","steve","longtail", nameProm];
+
+nameProm()
+console.log("The Monkey brothers " +
+				nameBros + 
+				" all had to sit down for dinner time.");
