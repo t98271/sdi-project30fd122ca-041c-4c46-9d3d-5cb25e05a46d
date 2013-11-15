7364ee305cb2b8566c89e74ec8d2b7c53f509616
Avery Bergara <maulventress@fullsail.edu>
2013-11-14 20:25:30 -0600

    done

--- rename-me.js
+++ rename-me.js
@@ -39,7 +39,6 @@ console.log("Bananas eaten " + bansEat);
 var numEat = 2;
 var nameProm = prompt("What should one of the brothers names be?", "john")
 var nameBros = ["chip","steve","longtail", nameProm];
-var dessert;
 var eatBans = function() {
 	for (var i = 0; i < nameBros.length; i++) {
 		console.log(nameBros[i] + " has eaten " + i + " of his bananas");
@@ -47,14 +46,9 @@ var eatBans = function() {
 }; 
 var have = confirm("Do we have dessert?")
 var dessert = confirm("Did all the monkeys eat enough to get Dessert. OK for yes, Cancel for no.");
-				
-
 var results = function (have,dessert){
-	
-	
 	if (have === true) {
 		console.log("Yum! we have dessert if you ate enough!");
-		
 			if (dessert === true){
 				console.log("Yay! Everyone gets dessert!");
 			} else {
@@ -64,14 +58,19 @@ var results = function (have,dessert){
 			console.log("Sorry we dont have any desert!");
 	};
 };	
+var oneDone = nameBros.pop()
+var keepEat = function (oneDone){
+	
+}
+
 
 console.log("The Monkey brothers " +
 				nameBros + 
 				" all had to sit down for dinner time.");
-
 eatBans();
 
 console.log(" To get desert the monkey must eat atleast" + numEat + " Bananas.");
 
 results(dessert)
 				
+console.log("since " + nameBros.pop() + "ate the most he can go!");
